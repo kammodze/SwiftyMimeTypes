1132b81ba6c8a3cab63d157eaaff227552712ec2
Kamil Modzelewski <kammodze@gmail.com>
2021-05-07 00:35:42 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "SwiftyMimeTypes",
+    platforms: [
+        .iOS(.v8)
+    ],
+    products: [
+        .library(name: "SwiftyMimeTypes", targets: ["SwiftyMimeTypes"])
+    ],
+    targets: [
+        .target(name: "SwiftyMimeTypes", dependencies: [], path: "SwiftyMimeTypes", resources: [.process("Assets")])
+    ]
+)
